325cf7cff2a9763afd23d865f63a91b25219a467
zhaoce <17631807110@163.com>
2023-01-07 00:07:20 +0800

    test

---
+++ a.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: test
+ * @Author: zhaoce
+ * @Developer: zhaoce
+ * @Date: 2023-01-07 00:07:20
+ * @LastEditTime: 2023-01-07 00:07:20
+ */
+
--- src/config/injectGitLog.js
+++ src/config/injectGitLog.js
@@ -1,16 +1,12 @@
 /*
- * @Description: 配置 husky
+ * @Description: test
  * @Author: zhaoce
  * @Developer: zhaoce
  * @Date: 2023-01-07 00:05:35
- * @LastEditTime: 2023-01-07 00:05:35
+ * @LastEditTime: 2023-01-07 00:07:20
  */
 
 /* 自动注入 git 信息 */
-
-// const dayjs = require("dayjs");
-// const fs = require("fs");
-// const { execSync } = require("child_process");
 import dayjs from "dayjs";
 import * as fs from "fs";
 import ChildProcess from "child_process";
